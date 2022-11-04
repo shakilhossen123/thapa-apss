80ebd90ac208aba91edb9efcec8ed4f4ad3efa6c
shakilhossen123 <mdshakilhossen736420@gmail.com>
2022-11-04 19:47:54 +0600

    card style and hover

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/Card';
+import './index.css';
+
+
+ReactDOM.render(
+  <>cd
+
+    <h1 className='hading-style'>List of top 5 Netflix Series in 2020</h1>
+
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Orignal Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80990668?source=25"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Orignal Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80990668?source=25"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Orignal Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80990668?source=25"
+    />
+  </>,
+  document.getElementById('root')
+)
